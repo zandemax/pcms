48975c7ea4c3eb7e4d375e81bd62d3d66c208c8e
Maximilian Zander <maximilian.zander@zmnet.de>
2017-07-30 16:36:53 +0200

    Fixed Controlbar icon color change

--- ui/components/controlbar.py
+++ ui/components/controlbar.py
@@ -4,3 +4,17 @@ class ControlBar(Widget):
 
     def __init__(self, **args):
         super().__init__()
+
+    def set_active_icon(self, screen):
+        if screen = 'homescreen':
+            self.ids.home.color = 1,1,1,1
+            self.ids.phone.color = .7,.7,.7,1
+            self.ids.music.color = .7,.7,.7,1
+        if screen = 'phonebook':
+            self.ids.home.color = .7,.7,.7,1
+            self.ids.phone.color = 1,1,1,1
+            self.ids.music.color = .7,.7,.7,1
+        if screen = 'music':
+            self.ids.home.color = .7,.7,.7,1
+            self.ids.phone.color = .7,.7,.7,1
+            self.ids.music.color = 1,1,1,1
