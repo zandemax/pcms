e97eb31ac1c6d963e7214d2deee0a3404673988a
Maximilian Zander <maximilian.zander@zmnet.de>
2017-08-10 18:13:33 +0200

    Added some icons

--- ui/components/bar/controlbar.py
+++ ui/components/bar/controlbar.py
@@ -11,11 +11,19 @@ class ControlBar(Widget):
             self.ids.home.color = 1,1,1,1
             self.ids.phone.color = .7,.7,.7,1
             self.ids.music.color = .7,.7,.7,1
+            self.ids.settings.color = .7,.7,.7,1
         if screen == 'contactscreen':
             self.ids.home.color = .7,.7,.7,1
             self.ids.phone.color = 1,1,1,1
             self.ids.music.color = .7,.7,.7,1
+            self.ids.settings.color = .7,.7,.7,1
         if screen == 'musicscreen':
             self.ids.home.color = .7,.7,.7,1
             self.ids.phone.color = .7,.7,.7,1
             self.ids.music.color = 1,1,1,1
+            self.ids.settings.color = .7,.7,.7,1
+        if screen == 'settingsscreen':
+            self.ids.home.color = .7,.7,.7,1
+            self.ids.phone.color = .7,.7,.7,1
+            self.ids.settings.color = 1,1,1,1
+            self.ids.music.color = .7,.7,.7,1
