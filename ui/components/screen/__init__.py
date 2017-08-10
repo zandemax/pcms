b961f89c3f4ec8824b1406dcf4007c2fa28f30c4
Maximilian Zander <maximilian.zander@zmnet.de>
2017-08-10 18:13:49 +0200

    Added settings

--- ui/components/screen/__init__.py
+++ ui/components/screen/__init__.py
@@ -4,3 +4,4 @@ from ui.components.screen.contactscreen import ContactScreen
 from ui.components.screen.homescreen import HomeScreen
 from ui.components.screen.musicscreen import MusicScreen
 from ui.components.screen.phonescreen import PhoneScreen
+from ui.components.screen.settingsscreen import SettingsScreen
